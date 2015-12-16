ddff74c193649ccbc641ba5cf8ef92c9bb5a2673
Ivan Ortega Alba <ivanortegaalba@gmail.com>
2015-12-16 14:56:38 +0100

    deploy

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 
 sudo pip install -r socialpoint/socialpoint/requirements.txt
+virtualenv socialpoint-virtualenv
+cd socialpoint-virtualenv
+pip install -r ../socialpoint/socialpoint/requirements.txt
 
 echo "open README.md to see how to start the project"
